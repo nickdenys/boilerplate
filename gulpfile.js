62d6fe17102de257a7edee7ad24fff0d0947f462
Nick Denys <nickdenys@gmail.com>
2016-06-08 22:29:10 +0200

    Cleaned and optimised Gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -13,23 +13,19 @@ var util          = require('gulp-util');
 var watch         = require('gulp-watch');
 var rimraf        = require('rimraf');
 
-// Define paths & files.
-var paths       = {};
-paths.assets    = './assets/';
-paths.dist      = './dist/';
-paths.js        = paths.assets + 'js/';
-paths.sass      = paths.assets + 'scss/';
-paths.css       = paths.dist + 'css/';
-paths.distJs    = paths.dist + 'js/';
-paths.distJsVendor = paths.dist + 'js/vendor/';
+// Define path & files.
+var path        = {};
+path.assets     = './assets/';
+path.dist       = './dist/';
 var files       = {};
-files.css       = paths.dist + 'css/**/*.css';
-files.minCss    = paths.dist + 'css/**/*.min.css';
-files.sass      = paths.assets + 'scss/**/*.scss';
-files.js        = paths.assets + 'js/**/*.js';
-files.jsVendor  = paths.assets + 'js/vendor/**/*.js';
-files.distJs    = paths.dist + 'js/**/*.js';
-files.images    = paths.assets + 'images/*.{jpg,png,svg,gif}';
+files.sass      = path.assets + 'scss/**/*.scss';
+files.js        = path.assets + 'scripts/**/*.js';
+files.vndr      = path.assets + 'scripts/vendor/**/*.js';
+files.imgs      = path.assets + 'images/*.{jpg,png,svg,gif}';
+files.css       = path.dist + 'css/**/*.css';
+files.cssMin    = path.dist + 'css/**/*.min.css';
+files.jsDist    = path.dist + 'scripts/**/*.js';
+files.jsDistMin = path.dist + 'scripts/**/*.min.js';
 
 // Default task when calling Gulp.
 gulp.task('default', ['build:dev']);
@@ -43,8 +39,8 @@ gulp.task('build', ['clean', 'min', 'images'], function() {
 });
 
 // Deletes the build folder entirely.
-gulp.task('clean', function(done) {
-  rimraf(paths.dist, done);
+gulp.task('clean', function(cb) {
+  rimraf(path.dist, cb);
 });
 
 // Watch and compile changed .scss files.
@@ -60,9 +56,9 @@ gulp.task('watch', function() {
   }
 });
 
-// Compile .scss files.
-gulp.task('sass', function(done) {
-  gulp.src(files.sass)
+// Compile .scss files. Provide callback for min:css
+gulp.task('sass', function() {
+  return gulp.src(files.sass)
     .pipe(sass())
     .on('error', sass.logError)
     .pipe(autoprefixer({
@@ -72,47 +68,48 @@ gulp.task('sass', function(done) {
         'opera 12'
       ]
     }))
-    .pipe(gulp.dest(paths.css))
-    .pipe(livereload())
-    .on('end', done);
+    .pipe(gulp.dest(path.dist + 'css'))
+    .pipe(livereload());
 });
 
 // Minify files.
 gulp.task('min', ['min:css', 'min:js']);
-// Minify compiled CSS files.
+// Minify compiled CSS files, ignore already minified files.
 gulp.task('min:css', ['sass'], function() {
-  gulp.src([files.css, '!' + files.minCss])
+  return gulp.src([files.css, '!' + files.cssMin])
     .pipe(cssmin())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest(paths.css))
+    .pipe(gulp.dest(path.dist + 'css'));
 });
-// Minify the JS files.
+// Minify our own JS files, ignore vendor files.
 gulp.task('min:js', ['jshint', 'vendor:js'], function() {
-  gulp.src([files.js, '!' + files.jsVendor])
+  return gulp.src([files.jsDist, '!' + files.jsDistMin, '!' + files.vndr])
     .pipe(uglify())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest(paths.distJs))
+    .pipe(gulp.dest(path.dist + 'scripts'));
 });
-// Run JSHint and move the files to the dist folder.
+
+// Run JSHint on custom files and move these to the dist folder.
 gulp.task('jshint', function() {
-  return gulp.src([files.js, '!' + files.jsVendor])
+  return gulp.src([files.js, '!' + files.vndr])
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(gulp.dest(paths.distJs));
+    .pipe(gulp.dest(path.dist + 'scripts'))
+    .pipe(livereload());
 });
 // Move the vendor files to the dist folder.
 gulp.task('vendor:js', function() {
-  gulp.src(files.jsVendor)
-    .pipe(gulp.dest(paths.distJsVendor));
+  return gulp.src(files.vndr)
+    .pipe(gulp.dest(path.dist + 'scripts/vendor'));
 });
 
 // Run lossless compression on all the images.
 gulp.task('images', function() {
-  return gulp.src(files.images)
+  return gulp.src(files.imgs)
     .pipe(imagemin({
       progressive: true,
       interlaced: true,
       svgoPlugins: [{removeUnknownsAndDefaults: false}, {cleanupIDs: false}]
     }))
-    .pipe(gulp.dest(paths.dist + 'images'));
+    .pipe(gulp.dest(path.dist + 'images'));
 });
